48ba05788b337a4384f2ef2caf6f4e3f21ce0ee5
Andres Perez <1676612+andresperezl@users.noreply.github.com>
2024-04-05 15:22:55 -0400

    double check pip

--- linux_deps.sh
+++ linux_deps.sh
@@ -2,11 +2,11 @@
 
 RELEASE=$(cat /etc/lsb-release | grep DISTRIB_RELEASE| cut -f2 -d'=' | cut -f1 -d'.')
 
-sudo apt update
+sudo apt-get update
 
 if [ $RELEASE -lt 23 ]; then
     which python3 || sudo apt-get install -y python3 python3-pip
-    python3 -m pip install --user pipx
+    python3 -m pip install --user pipx || sudo apt-get install -y python3-pip && python3 -m pip install --user pipx
     python3 -m pipx ensurepath
     sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
 else
