01edb076d3372134f355ed75d48e4e7ae1cc024f
Andres Perez <1676612+andresperezl@users.noreply.github.com>
2024-04-05 15:18:59 -0400

    install pyhton3

--- linux_deps.sh
+++ linux_deps.sh
@@ -5,11 +5,12 @@ RELEASE=$(cat /etc/lsb-release | grep DISTRIB_RELEASE| cut -f2 -d'=' | cut -f1 -
 sudo apt update
 
 if [ $RELEASE -lt 23 ]; then
+    which python3 || sudo apt-get install python3
     python3 -m pip install --user pipx
     python3 -m pipx ensurepath
     sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
 else
-    sudo apt install pipx
+    sudo apt-get install pipx
     pipx ensurepath
     sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
 fi
